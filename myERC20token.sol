3f7f20922fc634f49245d2e5afba4f7864a52472
Wisdom Iruemiobe <100282351+Remzeey@users.noreply.github.com>
2022-03-18 10:53:36 +0000

    ERC20 token etherscan link

---
+++ myERC20token.sol
@@ -0,0 +1,1 @@
+https://rinkeby.etherscan.io/tx/0x96c0d7bb34b606e2a137c22f9f8b0b250c277d3b1fec8b4790ebca28e458edab
--- myERCtoken.sol
+++
@@ -1,1 +0,0 @@
-https://rinkeby.etherscan.io/tx/0xf577c849e75c03ebadb78aa8678df57f7aaf408706abf1b18af31f3b4f8f609e
